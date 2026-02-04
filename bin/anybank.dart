7b1c6c2ab0bc75eafd84ded3a64ad9312bbf86cb
EdsonRego <edsonxrego@gmail.com>
2026-02-04 20:31:20 -0300

    Polimorfismo e Heranca

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -5,6 +5,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+  ContaPoupanca contaDenise = ContaPoupanca("Chris", 4000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
@@ -15,4 +17,12 @@ void main() {
   contaRoberta.receber(3000);
   contaMatheus.receber(500);
   contaMatheus.enviar(200);
+
+  contaChris.imprimiSaldo();
+  contaChris.enviar(4300);
+
+  contaDenise.imprimiSaldo();
+  contaDenise.enviar(4300);
+  contaDenise.calculaRendimento();
+  contaDenise.imprimiSaldo();
 }
