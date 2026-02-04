aea7266ec9ab38fac18b659cdf7747f9e5d3e3b0
EdsonRego <edsonxrego@gmail.com>
2026-02-04 20:33:23 -0300

    Classe abstrata

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,20 +3,8 @@ import 'dart:ffi';
 import 'package:anybank/conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaRoberta = Conta("Roberta", 2000);
   ContaCorrente contaChris = ContaCorrente("Chris", 4000);
   ContaPoupanca contaDenise = ContaPoupanca("Chris", 4000);
-
-  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
-
-  for (Conta conta in contas) {
-    conta.imprimiSaldo();
-  }
-
-  contaRoberta.receber(3000);
-  contaMatheus.receber(500);
-  contaMatheus.enviar(200);
 
   contaChris.imprimiSaldo();
   contaChris.enviar(4300);
